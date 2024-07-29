5b5ca5da6625a141ee3509a85b1a2c6ae4bec2db
Wasay Khan <wasaykhan2017@hotmail.com>
2024-07-29 14:54:02 +0500

    first commit, added code in main.cpp to load the source file given as arguments

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+
+
+int main(int argc, char** argv){
+    
+    std::ifstream file;
+    std::string filePath = argv[1];
+    file.open(filePath);
+
+
+    if(file.is_open()){
+        std::string line;
+        while(getline(file, line)){
+            std::cout << line << std::endl;
+        }
+    return 0;
+}
