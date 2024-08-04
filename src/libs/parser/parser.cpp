35bb5f1fd95655858c4f493136464d0340113510
Wasay Khan <wasaykhan2017@hotmail.com>
2024-08-04 15:37:00 +0500

    added code to parser.cpp

---
+++ src/libs/parser/parser.cpp
@@ -0,0 +1,41 @@
+#include "parser.h"
+
+void Parser::ParseExpression(){
+    parseAssignmentExpression();
+       if (currentToken.type != Token::EOF) {
+           std::cerr << "Unexpected token: " << currentToken.value << std::endl;
+           exit(1);
+       }
+}
+
+void Parser::parseAssignmentExpression() {
+    parseTerm();
+    if (currentToken.type == Token::ASSIGN) {
+        currentToken = lexer.nextToken();
+        std::cout << "Assignment detected!" << std::endl;
+        parseAssignmentExpression();
+    }
+}
+
+void Parser::parseTerm() {
+    parseFactor();
+    while (currentToken.type == Token::PLUS || currentToken.type == Token::MINUS) {
+        Token op = currentToken;
+        currentToken = lexer.nextToken();
+        parseFactor();
+        std::cout << "Term with operator: " << op.value << std::endl;
+    }
+}
+
+void Parser::parseFactor() {
+    if (currentToken.type == Token::IDENTIFIER) {
+        std::cout << "Identifier: " << currentToken.value << std::endl;
+        currentToken = lexer.nextToken();
+    } else if (currentToken.type == Token::NUMBER) {
+        std::cout << "Number: " << currentToken.value << std::endl;
+        currentToken = lexer.nextToken();
+    } else {
+        std::cerr << "Unexpected token: " << currentToken.value << std::endl;
+        exit(1);
+    }
+}
